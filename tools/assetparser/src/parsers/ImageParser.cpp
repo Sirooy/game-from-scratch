0dda9015ebe5adc519347d4d174ddd0072debda6
Siroy <victor_tob8@hotmail.com>
2025-03-27 12:04:13 +0100

    Added format conversion to ImageParser

--- tools/assetparser/src/parsers/ImageParser.cpp
+++ tools/assetparser/src/parsers/ImageParser.cpp
@@ -1,5 +1,5 @@
 #include "ImageParser.hpp"
-#include <cstdint>
+#include <memory>
 #include <fstream>
 #include <stdexcept>
 #include "../BinaryWrite.hpp"
@@ -8,13 +8,13 @@
 void ImageParser::ParseFile(const std::string& inputFile, 
     const std::string& outputFile)
 {
-    int32_t width       { 0 };
-    int32_t height      { 0 };
-    int32_t numChannels { 0 };
+    int32_t width            { 0 };
+    int32_t height           { 0 };
+    int32_t inputNumChannels { 0 };
 
-    uint8_t* data = stbi_load(inputFile.c_str(), &width, &height, &numChannels, 0);
+    uint8_t* inputData = stbi_load(inputFile.c_str(), &width, &height, &inputNumChannels, 0);
     
-    if(data == nullptr)
+    if(inputData == nullptr)
         throw std::runtime_error("Error parsing file \"" + inputFile + 
             "\" - " + stbi_failure_reason());
     
@@ -22,14 +22,115 @@ void ImageParser::ParseFile(const std::string& inputFile,
     os.open(outputFile, std::ios::out | std::ios::binary | std::ios::trunc);
 
     if(!os.is_open())
+    {
+        stbi_image_free(inputData);
         throw std::runtime_error("Could not open file \"" + outputFile + 
             "\" to write ");
-
+    }
+    
+    ImageFormat inputFormat   = GetStbiFormatFromChannels(inputNumChannels);
+    ImageFormat outputFormat  = inputFormat; //TODO: Read input format from config file
+    int32_t outputNumChannels = GetNumChannelsFromFormat(outputFormat);
+    
     WriteBytes(os, static_cast<uint16_t>(width));
     WriteBytes(os, static_cast<uint16_t>(height));
-    WriteBytes(os, static_cast<uint8_t>(numChannels));
-    WriteBytes(os, data, width * height * numChannels);
-    os.close();
+    WriteBytes(os, static_cast<uint8_t>(outputNumChannels)); //TODO: Write the format instead of the number of channels
 
-    stbi_image_free(data);
+    if(inputFormat == outputFormat)
+    {
+        WriteBytes(os, inputData, width * height * inputNumChannels);
+    }
+    else
+    {
+        WriteImageWithDifferentFormat(os, inputData, inputFormat,
+            outputFormat, width, height);
+    }
+
+    os.close();
+    stbi_image_free(inputData);
+}
+
+void ImageParser::WriteImageWithDifferentFormat(std::ofstream& os, uint8_t* inputData, 
+    ImageFormat inputFormat, ImageFormat outputFormat, int32_t width, int32_t height)
+{
+    int32_t inputNumChannels              = GetNumChannelsFromFormat(inputFormat);
+    int32_t outputNumChannels             = GetNumChannelsFromFormat(outputFormat);
+    int32_t numPixels                     = width * height;
+    int32_t outputSize                    = numPixels * outputNumChannels;
+    std::unique_ptr<uint8_t[]> outputData = std::make_unique<uint8_t[]>(outputSize);
+
+    uint8_t* inputPtr          = inputData;
+    uint8_t* outputPtr         = outputData.get();
+    int32_t minChannelsToCopy  = std::min(inputNumChannels, outputNumChannels);
+    const int32_t* inputTable  = FormatChannelIndices[static_cast<int32_t>(inputFormat)];
+    const int32_t* outputTable = FormatChannelIndices[static_cast<int32_t>(outputFormat)];
+
+    for(int32_t i = 0;i < numPixels; ++i)
+    {
+        int32_t channelIndex = 0;
+        //Copy the minimun number of channels
+        while(channelIndex < minChannelsToCopy)
+        {
+            outputPtr[outputTable[channelIndex]] = inputPtr[inputTable[channelIndex]];
+            channelIndex++;
+        }
+
+        //Assign default values if the output format has more channels
+        //than the input format
+        while(channelIndex < outputNumChannels)
+        {
+            outputPtr[outputTable[channelIndex]] = DefaultChannelValues[channelIndex];
+        }
+
+        //Advance to the next pixel
+        inputPtr  += inputNumChannels;
+        outputPtr += outputNumChannels;
+    }
+
+    WriteBytes(os, outputData.get(), outputSize);
+}
+
+int32_t ImageParser::GetNumChannelsFromFormat(ImageFormat format)
+{
+    switch(format)
+    {
+        case ImageFormat::R:    return 1;
+        case ImageFormat::RG:
+        case ImageFormat::GR:   return 2;
+        case ImageFormat::RGB:
+        case ImageFormat::BGR:  return 3;
+        case ImageFormat::RGBA: 
+        case ImageFormat::BGRA:
+        case ImageFormat::ARGB:
+        case ImageFormat::ABGR: return 4;
+        default: throw std::runtime_error("Unsupported format");
+    }
+}
+
+ImageFormat ImageParser::GetFormatFromString(const std::string& str)
+{
+    if(str == "r")    return ImageFormat::R;
+    if(str == "rg")   return ImageFormat::RG;
+    if(str == "gr")   return ImageFormat::GR;
+    if(str == "rgb")  return ImageFormat::RGB;
+    if(str == "bgr")  return ImageFormat::BGR;
+    if(str == "rgba") return ImageFormat::RGBA;
+    if(str == "bgra") return ImageFormat::BGRA;
+    if(str == "argb") return ImageFormat::ARGB;
+    if(str == "abgr") return ImageFormat::ABGR;
+
+    throw std::runtime_error("Invalid channel format \"" + str + "\"");
+}
+
+ImageFormat ImageParser::GetStbiFormatFromChannels(int32_t numChannels)
+{
+    switch (numChannels)
+    {
+        case 1: return ImageFormat::R;
+        case 2: return ImageFormat::RG;
+        case 3: return ImageFormat::RGB;
+        case 4: return ImageFormat::RGBA;
+    }
+
+    throw std::runtime_error("Invalid number of channels");
 }
--- tools/assetparser/src/parsers/ImageParser.hpp
+++ tools/assetparser/src/parsers/ImageParser.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <limits>
 #include <string>
 #include <vector>
+#include <cstdint>
 #include "BaseParser.hpp"
 
 /* ##### FORMAT ##### */
@@ -12,6 +14,29 @@ uint8_t   - Num channels
 uint8_t[] - [Width * Height * Num channels] Image bytes (R|RG|RGB|RGBA)
 */
 /* ##### ###### ##### */
+
+enum class ChannelIndex
+{
+    R = 0,
+    G = 1,
+    B = 2,
+    A = 3,
+    NumChannels
+};
+
+enum class ImageFormat : uint8_t
+{
+    R = 0,
+    RG,
+    GR,
+    RGB,
+    BGR,
+    RGBA,
+    BGRA,
+    ARGB,
+    ABGR,
+    NumFormats
+};
 
 class ImageParser : public BaseParser
 {
@@ -29,6 +54,38 @@ public:
     }
 
 protected:
+    void WriteImageWithDifferentFormat(std::ofstream& os, uint8_t* inputData, 
+        ImageFormat inputFormat, ImageFormat outputFormat, int32_t width, int32_t height);
+
+    static int32_t GetNumChannelsFromFormat(ImageFormat format);
+    static ImageFormat GetFormatFromString(const std::string& str);
+    static ImageFormat GetStbiFormatFromChannels(int32_t numChannels);
+
+protected:
     inline static const std::vector<std::string> m_InputExtensions { "png", "jpg", "jpeg", "bmp", "gif", "tga", "psd" };
     inline static constexpr std::string m_OutputExtension          { "img" };
+    //Default values to assign when converting from a format with less channels than the output format (Ex. R -> RGBA)
+    inline static constexpr int32_t DefaultChannelValues[static_cast<int32_t>(ChannelIndex::NumChannels)]
+    {
+      //R  G  B  A
+        0, 0, 0, std::numeric_limits<uint8_t>::max()
+    };
+    //Table indicating the position that a certain channel index occupies in a specific format
+    //(Ex. In the format ARGB, The channel index R(= 0) maps to the position/index 1 in the format)
+    //This table is used for conversions between image formats
+    inline static constexpr int32_t FormatChannelIndices
+        [static_cast<int32_t>(ImageFormat::NumFormats)][static_cast<int32_t>(ChannelIndex::NumChannels)]
+    { 
+        //R  G  B  A
+        { 0, 0, 0, 0}, //R
+        { 0, 1, 0, 0}, //RG
+        { 1, 0, 0, 0}, //GR
+        { 0, 1, 2, 0}, //RGB
+        { 2, 1, 0, 0}, //BGR
+        { 0, 1, 2, 3}, //RGBA
+        { 2, 1, 0, 3}, //BGRA
+        { 1, 2, 3, 0}, //ARGB
+        { 3, 2, 1, 0}  //ABGR
+    };
 };
+
